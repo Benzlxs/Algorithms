7de7130901cd323eb63a02cce0de2cbe4526161a
benzlxs <xuesong.li@unsw.edu.au>
2020-10-06 17:25:56 +1100

    leetcode

---
+++ exercise/leetcode/intersection_nodes.py
@@ -0,0 +1,25 @@
+"""
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+"""
+
+
+
+## recrusive
+class Solution:
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if headA == None or headB == None:
+            return None
+
+        A_pointer = headA
+        B_pointer = headB
+
+        while A_pointer != B_pointer:
+            A_pointer = headB if A_pointer == None else A_pointer.next
+            B_pointer = headA if B_pointer == None else B_pointer.next
+
+        return A_pointer
---
+++ exercise/leetcode/remove_from_end.py
@@ -0,0 +1,21 @@
+"""
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Using two pointers
+"""
+
+
+
+# use two points
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        fast = slow = head
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
